5449fee328de670b4a92c65a8da7e56cdcdabfed
Ilya Simpson <elliot@p-s.co.nz>
2025-04-01 19:04:22 +1300

    Implement `usethis badge usethis`

--- tests/usethis/_interface/test_interface_badge.py
+++ tests/usethis/_interface/test_interface_badge.py
@@ -73,3 +73,26 @@ class TestPreCommit:
 
         # Assert
         assert result.exit_code == 0, result.output
+
+
+class TestUseThis:
+    def test_add(self, tmp_path: Path):
+        # Act
+        runner = CliRunner()
+        with change_cwd(tmp_path):
+            result = runner.invoke(app, ["usethis"])
+
+        # Assert
+        assert result.exit_code == 0, result.output
+
+    def test_remove(self, tmp_path: Path):
+        # Arrange
+        (tmp_path / "README.md").write_text("")
+
+        # Act
+        runner = CliRunner()
+        with change_cwd(tmp_path):
+            result = runner.invoke(app, ["usethis", "--remove"])
+
+        # Assert
+        assert result.exit_code == 0, result.output
